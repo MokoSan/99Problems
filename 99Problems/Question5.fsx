8cde741feb49fca83c7ea5c074c6fce1e3e83a49
MokoSan <mukund.sharma92@gmail.com>
2016-09-12 19:06:25 -0400

    Added question 5

---
+++ 99Problems/Question5.fsx
@@ -0,0 +1,15 @@
+﻿// Question 5
+// Reverse a list
+
+let rec reverse0 ( l : 'a List ) : 'a List = 
+    match l with
+    | [] -> []
+    | [ x ] -> [ x ]
+    | x :: xs -> reverse0 ( xs ) @ [ x ]
+
+let reverse1 ( l : 'a List ) : 'a List = 
+    let rec loop ( acc : 'a List ) ( l' : 'a List ) : 'a List =
+        match l' with
+        | [] -> acc
+        | x :: xs -> loop ( x :: acc ) xs 
+    loop [] l  
