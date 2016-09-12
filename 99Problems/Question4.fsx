70be982bf36237d533980873c8eaeee09631c5db
MokoSan <mukund.sharma92@gmail.com>
2016-09-12 19:01:36 -0400

    Added the 4th question

---
+++ 99Problems/Question4.fsx
@@ -0,0 +1,14 @@
+﻿// Question 4
+// Count elements in a list
+
+let rec listLength0 ( l : 'a List ) : int =
+    match l with
+    | [] -> 0
+    | x :: xs -> 1 + listLength0( xs )
+
+let listLength1 ( l : 'a List ) : int = 
+    let rec loop ( acc : int ) ( l' : 'a List ) : int = 
+        match l' with
+        | [] -> acc
+        | x :: xs -> loop ( acc + 1 ) xs
+    loop 0 l
