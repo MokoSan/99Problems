4fb26a5afae8c2a876b40e181dd984f788efad86
MokoSan <mukund.sharma92@gmail.com>
2016-09-12 18:47:25 -0400

    Updated the first question and finished 2nd question

--- 99Problems/Question1.fsx
+++ 99Problems/Question1.fsx
@@ -1,15 +1,19 @@
 ﻿// Question 1
 // Last element of a list
 
-exception EmptyListError of string
+exception EmptyListException of string
 
 let rec lastElement0 ( l : 'a List ) : 'a = 
     match l with
     | x :: [] -> x
     | x :: xs -> lastElement0( xs ) 
-    | _ -> raise (EmptyListError("Empty List as an input!")) 
+    | _ -> raise (EmptyListException("Empty List as an input!")) 
 
 let lastElement1 ( l : 'a List ) : 'a = 
     match l with
-    | [] -> raise (EmptyListError("Empty List as an input!")) 
+    | [] -> raise (EmptyListException("Empty List as an input!")) 
     | x -> x |> List.rev |> List.head
+
+(* Tests *)
+lastElement0 [1;2;3;] = 3 
+lastElement1 [1;2;3;] = 3
---
+++ 99Problems/Question2.fsx
@@ -0,0 +1,22 @@
+﻿// Question 2
+// Second Last Element of a List
+
+exception EmptyListException      of string
+exception OneElementListException of string
+
+let rec secondLastElement0 ( l : 'a List ) : 'a =    
+    match l with
+    | [] -> raise (EmptyListException("Empty list as an input"))
+    | [ x ] -> raise (OneElementListException("List too short to get the 2nd last element."))
+    | x :: _ :: [] -> x 
+    | _ :: xs -> secondLastElement0(xs)
+
+let secondLastElement1 ( l : 'a List ) : 'a = 
+    match l with
+    | [] -> raise (EmptyListException("Empty list as an input"))
+    | [ x ] -> raise (OneElementListException("List too short to get the 2nd last element."))
+    | x -> x |> List.rev |> List.tail |> List.head
+
+(* Tests *)
+secondLastElement0 [1;2;3;4] = 3 
+secondLastElement1 [1;2;3;4] = 3
