b440d1f7a9b80a0da1621cf687b7bf3df6c6eef8
MokoSan <mukund.sharma92@gmail.com>
2016-09-13 19:31:29 -0400

    Added q10

---
+++ 99Problems/Question10.fsx
@@ -0,0 +1,15 @@
+﻿// Question 10
+// Run-length encoding of a list.
+
+// we use the answer in Q9 to help us out.
+let packDuplicates0 l = 
+    let collectDuplicates x = function
+        | ( y :: xs ) :: ys when x = y -> ( x :: y :: xs ) :: ys
+        | xss -> [x] :: xss
+    List.foldBack collectDuplicates l []
+
+let encode0 ( list : 'a List ) : 'b List = 
+    list 
+    |> packDuplicates0 
+    |> List.map(fun xs -> List.length xs, List.head xs)
+
