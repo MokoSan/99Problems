1c785af3649f163f82282509a6b6f30ddd9fe579
MokoSan <mukund.sharma92@gmail.com>
2016-09-13 18:32:26 -0400

    Figured out question 7

--- 99Problems/Question7.fsx
+++ 99Problems/Question7.fsx
@@ -3,9 +3,11 @@
 
 type 'a node =
     | One of 'a 
-    | Many of 'a node list
+    | List of 'a node list
 
-let rec flattenList0 ( l : 'a List ) : 'a List = 
-    match l with
-    | One x -> [ x; ]
-    | Many x -> List.collect flattenList0 x
+let flattenList0 l =
+    let rec loop acc l' = 
+        match l' with
+            | One x -> x :: acc
+            | List x -> List.foldBack(fun x' acc -> loop acc x') x acc 
+    loop [] l
