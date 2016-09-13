b96f0338af045f9560d287fe990fc2c33bb17f4c
MokoSan <mukund.sharma92@gmail.com>
2016-09-13 19:16:50 -0400

    Added question 9

---
+++ 99Problems/Question9.fsx
@@ -0,0 +1,8 @@
+﻿// Question 9
+// Pack consecutive duplicates of list elements into sublists.
+
+let packDuplicates0 l = 
+    let collectDuplicates x = function
+        | ( y :: xs ) :: ys when x = y -> ( x :: y :: xs ) :: ys
+        | xss -> [x] :: xss
+    List.foldBack collectDuplicates l []
