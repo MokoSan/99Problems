1423a7ec85e2024dff0fb30df8bafacbc6c79eae
MokoSan <mukund.sharma92@gmail.com>
2016-09-12 21:44:51 -0400

    Added Question8

---
+++ 99Problems/Question8.fsx
@@ -0,0 +1,8 @@
+﻿// Question 8
+// Unique elements from a list
+
+let distinctList0 ( l : 'a List ) : 'a List =
+    List.distinct l
+
+let distinctList1 ( l : 'a List ) : 'a List =
+    l |> Set.ofList |> Set.toList 
