3f20a6219bc55ac52cd507878eafd99448ac57e5
MokoSan <mukund.sharma92@gmail.com>
2016-09-12 23:58:04 -0400

    Updated fsproj and Question7

---
+++ 99Problems/Question7.fsx
@@ -0,0 +1,11 @@
+﻿// Question 7
+// Flatten a nested list structure
+
+type 'a node =
+    | One of 'a 
+    | Many of 'a node list
+
+let rec flattenList0 ( l : 'a List ) : 'a List = 
+    match l with
+    | One x -> [ x; ]
+    | Many x -> List.collect flattenList0 x
