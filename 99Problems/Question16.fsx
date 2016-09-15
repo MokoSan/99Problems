b5d71dfc00a560c03795afad7d8cef1d70d1418a
MokoSan <mukund.sharma92@gmail.com>
2016-09-14 21:24:45 -0400

    Need to debug q16

--- 99Problems/Question16.fsx
+++ 99Problems/Question16.fsx
@@ -1,1 +1,20 @@
-﻿
+﻿// Question 16
+// Drop every N'th element from a list.
+
+let dropNthElement0 ( list : 'a List ) ( n : int ) : 'a List = 
+    let rec loop l n' =
+        match l, n' with
+        | [], _ -> []
+        | _ :: xs, 1 -> loop xs n'
+        | x :: xs, _ -> x :: loop xs ( n' - 1 ) 
+    loop list n
+
+let dropNthElement1 ( list : 'a List ) ( n : int ) : 'a List = 
+    [0..list.Length + 1] 
+    |> List.filter(fun i -> i % n <> 0 )
+    |> List.collect(fun x -> [ list.[x] ])
+
+let dropNthElement2 ( list : 'a List ) ( n : int ) : 'a List =
+    list |> List.mapi(fun i x -> (i + 1, x))
+         |> List.filter(fun (i, _) -> i % n <> 0 )
+         |> List.map snd
