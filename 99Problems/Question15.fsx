186bda2589ad5db241e7271b28cc234c8b964890
MokoSan <mukund.sharma92@gmail.com>
2016-09-14 20:37:44 -0400

    Added question 15

---
+++ 99Problems/Question15.fsx
@@ -0,0 +1,5 @@
+﻿// Question 15
+// Replicate the elements of a list a given number of times.
+
+let replicate0 ( list : 'a List ) ( n : int ) : 'a List =
+    list |> List.collect( List.replicate n )
