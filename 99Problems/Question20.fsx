af64a1a5f8f11a796f47d24cd4801d105b49baab
MokoSan <mukund.sharma92@gmail.com>
2016-09-15 22:26:36 -0400

    Adde d question20

---
+++ 99Problems/Question20.fsx
@@ -0,0 +1,8 @@
+﻿// Question 20
+// Remove the K'th element from a list
+
+let removeKthElement0 ( list : 'a List ) ( k : int ) : 'a List = 
+    if list = [] then []
+    else list |> List.mapi(fun i x -> i + 1, x)
+              |> List.filter(fun x -> fst (x) <> k)
+              |> List.map(fun x -> snd x)
