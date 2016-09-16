bc5683d2c78ffe2348efd57c7a6fd35af3b570e9
MokoSan <mukund.sharma92@gmail.com>
2016-09-15 22:50:02 -0400

    Added Q23 and 22

---
+++ 99Problems/Question22.fsx
@@ -0,0 +1,8 @@
+﻿// Question 22
+// Create a list containing all integers within a given range
+
+let listWithinARange0 ( a : int ) ( b : int ) : int List =
+    [ a .. b ]
+
+let listWithinARange1 ( a : int ) ( b : int ) : int List =
+    seq { for i in a .. b -> i } |> Seq.toList
---
+++ 99Problems/Question23.fsx
@@ -0,0 +1,2 @@
+﻿// Question 23
+// Extract a given number of randomly selected elements from a list.
