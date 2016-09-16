541623212f30e2e8c90128681ccb13b33e837800
MokoSan <mukund.sharma92@gmail.com>
2016-09-15 22:19:53 -0400

    Added question 18

--- 99Problems/Question18.fsx
+++ 99Problems/Question18.fsx
@@ -1,2 +1,7 @@
 ﻿// Question 18
+// Extract a slice from a list given i, j
 
+let sliceList0 ( list : 'a List ) ( i : int ) ( j : int ): 'a List =
+    list |> List.mapi(fun i x -> i + 1, x)
+         |> List.filter(fun x -> fst x < i && fst x > j) 
+         |> List.map(fun x -> snd x )
