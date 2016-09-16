c1d04ed844891270bdc5985bdf7dd0fa67d19b70
MokoSan <mukund.sharma92@gmail.com>
2016-09-15 22:34:06 -0400

    Added question 21

---
+++ 99Problems/Question21.fsx
@@ -0,0 +1,11 @@
+﻿// Question 21
+// Insert an element at a given position into a list
+
+exception EmptyListException of string
+
+let rec insertElementAt ( list : 'a List ) ( x : 'a ) ( i : int ) : 'a List = 
+    match list, i with
+    | [], 1 -> [ x ]
+    | [], _ -> raise (EmptyListException("list is empty!")) 
+    | _, 1 -> x :: list 
+    | y :: ys, n -> insertElementAt ys x (n - 1) 
